7b1f72689678c241076b234c6afb74fb21606efb
Crowdbotics <team@crowdbotics.com>
2020-11-20 01:23:44 +0000

    Update models

--- backend/toilet_finder_22828/wsgi.py
+++ backend/toilet_finder_22828/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toilet_finder_22828.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toilet_finder_22828.settings")
 
 application = get_wsgi_application()
