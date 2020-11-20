d30f710b1fbc48c54305b33cbdb913f6fbea6aae
Crowdbotics <team@crowdbotics.com>
2020-11-20 01:22:55 +0000

    Initial commit.

---
+++ backend/toilet_finder_22828/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for toilet_finder_22828 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toilet_finder_22828.settings')
+
+application = get_wsgi_application()
